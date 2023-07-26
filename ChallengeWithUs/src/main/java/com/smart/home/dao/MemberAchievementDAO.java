ab7fcb6726bd4c443b3a9e2fae029852b3c28c9e
Seo Minguk <132624647+orangejoa@users.noreply.github.com>
2023-07-26 09:19:59 +0900

    Update DAO files from 민국 project
    민국, DAO 파일 업데이트

---
+++ ChallengeWithUs/src/main/java/com/smart/home/dao/MemberAchievementDAO.java
@@ -0,0 +1,13 @@
+package com.smart.home.dao;
+
+import com.smart.home.dto.ChallengeDTO;
+import com.smart.home.dto.MemberAchievementDTO;
+
+public interface MemberAchievementDAO {
+    // 달성률 조회
+	int getAchievementRate(ChallengeDTO cDTO);
+	
+	// 달성률 업데이트(특정 조건에서 트리거가 작동할 때 자동으로 업데이트) 	
+	
+	// 달성률 삭제
+}
